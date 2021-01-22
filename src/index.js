d25bd0e8c3d9ca3a8621b93c17c69a713b9ecafd
Plofland <ploflan@gmail.com>
2021-01-22 12:53:12 -0500

    Set up store, provider and middleware on index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applymiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-import "./index.css";
-import App from "./App";
+import './index.css';
+import App from './App';
+import reducer from './reducers/index';
 
 const { worker } = require('./mocks/browser');
 worker.start();
 
-const rootElement = document.getElementById("root");
+const store = createStore(reducer, applyMiddleware(thunk, logger));
+
+const rootElement = document.getElementById('root');
 
 ReactDOM.render(
-    <App />, 
-    rootElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
 );
 
 //Task List:
